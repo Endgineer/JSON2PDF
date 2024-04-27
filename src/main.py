35e2f1e839d2aed220a722fa7edfd3ca1e2b3c8c
Endgineer <bm.alkhudairy@hotmail.com>
2024-04-27 03:35:24 -0400

    Minor refactor

--- src/main.py
+++ src/main.py
@@ -20,12 +20,11 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
-    logger = logging.getLogger()
     error_handler = errorhandler.ErrorHandler()
     stream_handler = logging.StreamHandler(stream=sys.stderr)
     stream_handler.setFormatter(logging.Formatter(fmt='[%(levelname)s]: %(message)s'))
-    logger.setLevel(logging.DEBUG if args.debug else logging.ERROR)
-    logger.addHandler(stream_handler)
+    logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.ERROR)
+    logging.getLogger().addHandler(stream_handler)
 
     if error_handler.fired: exit()
     subprocess.call([f'xelatex', f'{args.cv_blueprint_json_filepath}.tex'])
