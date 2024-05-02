a4868be29d2a74f19ced50eb989fb6fa07493e6a
Endgineer <bm.alkhudairy@hotmail.com>
2024-05-02 08:02:12 -0400

    Minor fix

--- src/lexer/routines/matchers.py
+++ src/lexer/routines/matchers.py
@@ -7,45 +7,45 @@ from lexer.components.Token import Token
 #      ██    ██    ██   ██ ██   ██    ██        ██  ██  ██ ██   ██    ██    ██      ██   ██ ██      ██   ██ 
 # ███████    ██    ██   ██ ██   ██    ██        ██      ██ ██   ██    ██     ██████ ██   ██ ███████ ██   ██ 
 
-def match_start(lexer_context: Context) -> bool:
-  if lexer_context.state != Context.State.START: return False
+def match_start(lexer_ctx: Context) -> bool:
+  if lexer_ctx.state != Context.State.START: return False
 
-  match(lexer_context.head):
+  match(lexer_ctx.head):
     case ' ':
-      lexer_context.token_start_idx += 1
+      lexer_ctx.token_start_idx += 1
     case '\n':
-      lexer_context.token_start_idx += 1
+      lexer_ctx.token_start_idx += 1
     case '\t':
-      lexer_context.token_start_idx += 1
+      lexer_ctx.token_start_idx += 1
     case '\r':
-      lexer_context.token_start_idx += 1
+      lexer_ctx.token_start_idx += 1
     case '{':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.LBRACE
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.LBRACE
     case '}':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.RBRACE
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.RBRACE
     case '[':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.LBRACKET
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.LBRACKET
     case ']':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.RBRACKET
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.RBRACKET
     case ':':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.COLON
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.COLON
     case ',':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.COMMA
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.COMMA
     case 'n':
-      lexer_context.token_len += 1
-      lexer_context.state = Context.State.NULL_AWAIT_U
+      lexer_ctx.token_len += 1
+      lexer_ctx.state = Context.State.NULL_AWAIT_U
     case '"':
-      lexer_context.token_len += 1
-      lexer_context.state = Context.State.STR_AWAIT_CHAR
+      lexer_ctx.token_len += 1
+      lexer_ctx.state = Context.State.STR_AWAIT_CHAR
     case _:
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.DISCARDED
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.DISCARDED
   
   return True
 
@@ -55,39 +55,39 @@ def match_start(lexer_context: Context) -> bool:
 # ██  ██ ██ ██    ██ ██      ██          ██  ██  ██ ██   ██    ██    ██      ██   ██ ██      ██   ██      ██ 
 # ██   ████  ██████  ███████ ███████     ██      ██ ██   ██    ██     ██████ ██   ██ ███████ ██   ██ ███████ 
 
-def match_null_u(lexer_context: Context) -> bool:
-  if lexer_context.state != Context.State.NULL_AWAIT_U: return False
+def match_null_u(lexer_ctx: Context) -> bool:
+  if lexer_ctx.state != Context.State.NULL_AWAIT_U: return False
 
-  if lexer_context.head == 'u':
-    lexer_context.token_len += 1
-    lexer_context.state = Context.State.NULL_AWAIT_L1
+  if lexer_ctx.head == 'u':
+    lexer_ctx.token_len += 1
+    lexer_ctx.state = Context.State.NULL_AWAIT_L1
   else:
-    lexer_context.token_len += 1
-    lexer_context.token_kind = Token.Kind.DISCARDED
+    lexer_ctx.token_len += 1
+    lexer_ctx.token_kind = Token.Kind.DISCARDED
   
   return True
 
-def match_null_l1(lexer_context: Context) -> bool:
-  if lexer_context.state != Context.State.NULL_AWAIT_L1: return False
+def match_null_l1(lexer_ctx: Context) -> bool:
+  if lexer_ctx.state != Context.State.NULL_AWAIT_L1: return False
 
-  if lexer_context.head == 'l':
-    lexer_context.token_len += 1
-    lexer_context.state = Context.State.NULL_AWAIT_L2
+  if lexer_ctx.head == 'l':
+    lexer_ctx.token_len += 1
+    lexer_ctx.state = Context.State.NULL_AWAIT_L2
   else:
-    lexer_context.token_len += 1
-    lexer_context.token_kind = Token.Kind.DISCARDED
+    lexer_ctx.token_len += 1
+    lexer_ctx.token_kind = Token.Kind.DISCARDED
   
   return True
 
-def match_null_l2(lexer_context: Context) -> bool:
-  if lexer_context.state != Context.State.NULL_AWAIT_L2: return False
+def match_null_l2(lexer_ctx: Context) -> bool:
+  if lexer_ctx.state != Context.State.NULL_AWAIT_L2: return False
 
-  if lexer_context.head == 'l':
-    lexer_context.token_len += 1
-    lexer_context.token_kind = Token.Kind.NULL
+  if lexer_ctx.head == 'l':
+    lexer_ctx.token_len += 1
+    lexer_ctx.token_kind = Token.Kind.NULL
   else:
-    lexer_context.token_len += 1
-    lexer_context.token_kind = Token.Kind.DISCARDED
+    lexer_ctx.token_len += 1
+    lexer_ctx.token_kind = Token.Kind.DISCARDED
   
   return True
 
@@ -97,17 +97,17 @@ def match_null_l2(lexer_context: Context) -> bool:
 #      ██    ██    ██   ██     ██  ██  ██ ██   ██    ██    ██      ██   ██ ██      ██   ██      ██ 
 # ███████    ██    ██   ██     ██      ██ ██   ██    ██     ██████ ██   ██ ███████ ██   ██ ███████ 
 
-def match_str_char(lexer_context: Context) -> bool:
-  if lexer_context.state != Context.State.STR_AWAIT_CHAR: return False
+def match_str_char(lexer_ctx: Context) -> bool:
+  if lexer_ctx.state != Context.State.STR_AWAIT_CHAR: return False
 
-  match(lexer_context.head):
+  match(lexer_ctx.head):
     case '"':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.STRING
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.STRING
     case '\\':
-      lexer_context.token_len += 1
-      lexer_context.token_kind = Token.Kind.DISCARDED
+      lexer_ctx.token_len += 1
+      lexer_ctx.token_kind = Token.Kind.DISCARDED
     case _:
-      lexer_context.token_len += 1
+      lexer_ctx.token_len += 1
   
   return True
