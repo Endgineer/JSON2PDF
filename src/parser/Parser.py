06d813fa0ab6fa8c5f4bbfee3058f8ab2130c79f
Endgineer <bm.alkhudairy@hotmail.com>
2024-05-09 05:18:41 -0400

    Fixed parser type hints

--- src/parser/Parser.py
+++ src/parser/Parser.py
@@ -1,6 +1,7 @@
 from parser.components.ParserContext import ParserContext
 from parser.routines.derivators import *
 from lexer.Lexer import Lexer
+from compiler.consts.typing import *
 
 class Parser():
   parser_ctx: ParserContext
@@ -8,9 +9,9 @@ class Parser():
   def __init__(self, lexer: Lexer):
     self.parser_ctx = ParserContext(lexer)
   
-  def parse_cv(self) -> list[tuple[str, list[str | list[tuple[str, str | list[str] | list[tuple[str, str]]]]]]]:
+  def parse_cv(self) -> SyntacticTypes.SectionsList:
     return derive_curriculumvitae(self.parser_ctx)
 
-  def parse_rf(self, filepath) -> list[tuple[str, list[tuple[str, str | list[str] | list[tuple[str, str]]]]]]:
+  def parse_rf(self, filepath) -> SyntacticTypes.ReferencedItemsList:
     self.parser_ctx.lexer.context_switch(filepath)
     return derive_references(self.parser_ctx)
