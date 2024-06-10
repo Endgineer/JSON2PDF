806274e18028b0b6d62f5de4304e031d9955c460
Endgineer <bm.alkhudairy@hotmail.com>
2024-06-10 05:03:36 -0400

    Added testing and first test

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 exe = json2cv
 
-windows: windows-build windows-clean
+windows: windows-build windows-clean windows-test
 
 windows-build:
 	poetry run pyinstaller -F --specpath .\release\spec --distpath .\ --workpath .\release\build --name $(exe) .\src\main.py
@@ -10,4 +10,4 @@ windows-clean:
 	rmdir /s /q .\release
 
 windows-test:
-	.\$(exe) .\samples\resume --footer --debug
+	cd .\test && python test.py
---
+++ test/test.py
@@ -0,0 +1,46 @@
+import subprocess
+import unittest
+import os
+
+EXP_LOG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'expected_logs')
+GEN_LOG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'generated_logs')
+IMG_JSN_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'image_jsons')
+
+
+
+class LogEqualityTest(unittest.TestCase):
+  def __init__(self, filename):
+    super(LogEqualityTest, self).__init__()
+    self.filebasename = os.path.splitext(os.path.basename(filename))[0]
+  
+  def assertGeneratedIsExpected(self):
+    expected_log_filepath = os.path.join(EXP_LOG_DIR, f'{self.filebasename}.debug.log')
+    generated_log_filepath = os.path.join(GEN_LOG_DIR, f'{self.filebasename}.debug.log')
+
+    if not os.path.exists(expected_log_filepath):
+      raise AssertionError(f'Test image at "{self.filebasename}.json" does not have a corresponding log file in the "expected_logs" directory.')
+    with open(generated_log_filepath, 'r', encoding='utf-8') as generated_log_file:
+      with open(expected_log_filepath, 'r', encoding='utf-8') as expected_log_file:
+        self.assertEqual(generated_log_file.read(), expected_log_file.read())
+  
+  def runTest(self):
+    subprocess.call(f'../json2cv ../image_jsons/{self.filebasename} --debug --interrupt', cwd=GEN_LOG_DIR)
+    self.assertGeneratedIsExpected()
+
+
+
+def test_all_suite() -> unittest.TestSuite:
+  suite = unittest.TestSuite()
+  if not os.path.isdir(GEN_LOG_DIR):
+    os.mkdir(GEN_LOG_DIR)
+
+  for filename in os.listdir(IMG_JSN_DIR):
+    if not os.path.isdir(filename):
+      suite.addTest(LogEqualityTest(filename))
+  
+  return suite
+
+
+
+if __name__ == '__main__':
+  unittest.TextTestRunner().run(test_all_suite())
