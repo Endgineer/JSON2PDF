a72a2c669e726bf0786c136d62413885790712b0
Endgineer <bm.alkhudairy@hotmail.com>
2024-09-08 06:43:10 -0400

    Removed stochastic and cluttery aspects of logger pattern

--- src/main/inilogger.cpp
+++ src/main/inilogger.cpp
@@ -3,7 +3,7 @@
 void IniLogger::log(spdlog::level::level_enum level, const std::string &message) {
   if(IniLogger::mainLogger == nullptr) {
     IniLogger::mainLogger = spdlog::stdout_logger_st("main").get();
-    IniLogger::mainLogger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [json2pdf] [%n] [%l] %v");
+    IniLogger::mainLogger->set_pattern("[%n %l] %v");
   }
 
   IniLogger::mainLogger->log(level, message);
