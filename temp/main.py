6f6bf7c454b8ed8253b6c958d479063dc3ee3982
Endgineer <bm.alkhudairy@hotmail.com>
2024-11-08 17:24:15 -0500

    Instantiated PhaseLogger in main

--- temp/main.py
+++ temp/main.py
@@ -1,4 +1,6 @@
 from boot.ArgParser import ArgParser
+from boot.PhaseLogger import PhaseLogger
 
 if __name__ == '__main__':
   args = ArgParser()
+  logger = PhaseLogger(args.json, args.abort, args.debug)
