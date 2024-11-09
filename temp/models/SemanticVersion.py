106a9c961a79deb858b73559d9ca665ca58dc8a6
Endgineer <bm.alkhudairy@hotmail.com>
2024-11-09 02:13:33 -0500

    Fixed SemanticVersion older_than function and refactored

--- temp/models/SemanticVersion.py
+++ temp/models/SemanticVersion.py
@@ -4,11 +4,10 @@ class SemanticVersion:
   patch: int
 
   def __init__(self, version: str) -> None:
-    major, minor, patch = version.split('.')
-    self.major = int(major)
-    self.minor = int(minor)
-    self.patch = int(patch)
+    self.major, self.minor, self.patch = map(int, version.split('.'))
+  
+  def as_tuple(self) -> tuple[int, int, int]:
+    return (self.major, self.minor, self.patch)
   
   def is_older_than(self, version: str) -> bool:
-    other = SemanticVersion(version)
-    return other.major > self.major or other.minor > self.minor or other.patch > self.patch
+    return self.as_tuple() < SemanticVersion(version).as_tuple()
